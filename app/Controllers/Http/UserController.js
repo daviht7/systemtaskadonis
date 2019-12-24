8c3b934dfeb27ddbd010ea9f50990c7e928ad7c7
Davi Holanda <daviht7@gmail.com>
2019-12-24 15:40:47 -0300

    auth jwt

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -7,15 +7,10 @@ class UserController {
   async store({
     request
   }) {
-
     const data = request.only(["username", "email", "password"])
-
     const user = await User.create(data)
-
     return user;
-
   }
-
 
 }
 
